739961e6d013125de49ceeb52d458cade1064226
ajaydewari <ajdewari@gmail.com>
2017-05-12 17:09:40 +0530

    Initial commit

---
+++ app/src/main/java/com/ajaysinghdewari/boundservicesample/MainActivity.java
@@ -0,0 +1,102 @@
+package com.ajaysinghdewari.boundservicesample;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnStartService, btnStopService, btnBindService, btnUnbindService, btnGetNumber;
+    ServiceConnection mServiceConnection;
+    MyService mService;
+    Intent mServiceIntent;
+    boolean isServiceBound;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnBindService= (Button) findViewById(R.id.bind_service);
+        btnUnbindService= (Button) findViewById(R.id.unbind_service);
+        btnStartService= (Button) findViewById(R.id.start_service);
+        btnStopService= (Button) findViewById(R.id.stop_service);
+        btnGetNumber= (Button) findViewById(R.id.get_number);
+        mServiceIntent=new Intent(this, MyService.class);
+
+        btnGetNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getRandomNumber();
+            }
+        });
+
+
+        btnStartService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(mServiceIntent);
+            }
+        });
+
+        btnStopService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(mServiceIntent);
+                       }
+        });
+
+        btnBindService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bindService();
+            }
+        });
+        btnUnbindService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            unBindService();
+            }
+        });
+    }
+private void bindService(){
+    if(mServiceConnection==null){
+        mServiceConnection= new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+                MyService.MyServiceBinder binder= (MyService.MyServiceBinder) iBinder;
+                mService=binder.getServiceInstance();
+            isServiceBound=true;
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName componentName) {
+                isServiceBound=false;
+            }
+        };
+    }
+    bindService(mServiceIntent, mServiceConnection, Context.BIND_AUTO_CREATE);
+}
+
+private void unBindService(){
+if(isServiceBound){
+    unbindService(mServiceConnection);
+    isServiceBound=false;
+}
+}
+
+private void getRandomNumber(){
+    if(isServiceBound){
+        Toast.makeText(MainActivity.this, "Random No is:"+mService.getRandomNumber(), Toast.LENGTH_LONG).show();
+    }else{
+        Toast.makeText(MainActivity.this, "Service is not bound to this activity ", Toast.LENGTH_LONG).show();
+    }
+}
+}
---
+++ app/src/main/java/com/ajaysinghdewari/boundservicesample/MyService.java
@@ -0,0 +1,79 @@
+package com.ajaysinghdewari.boundservicesample;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.IntDef;
+import android.util.Log;
+
+import java.util.Random;
+
+public class MyService extends Service {
+
+    private final static String TAG=MyService.class.getSimpleName();
+    private int mRandomNumber;
+    private boolean mIsRandomGeneratorIsOn;
+    private final static int MIN=0;
+    private final static int MAX=100;
+    private MyServiceBinder mServiceBinder=new MyServiceBinder();
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+//        throw new UnsupportedOperationException("Not yet implemented");
+        return mServiceBinder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, /*@IntDef(value = {Service.START_FLAG_REDELIVERY, Service.START_FLAG_RETRY}, flag = true)*/int flags, int startId) {
+
+        Log.d(TAG, "OnStartCommand() triggred, Thread_id= "+Thread.currentThread().getId());
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+           startRandomNumberGenerator();
+            }
+        }).start();
+        return START_STICKY;
+    }
+
+    protected int getRandomNumber(){
+        return mRandomNumber;
+    }
+    private void stopRandomNumberGenerator(){
+        mIsRandomGeneratorIsOn=false;
+    }
+    private void startRandomNumberGenerator(){
+        mIsRandomGeneratorIsOn=true;
+        int i=0;
+        while(mIsRandomGeneratorIsOn){
+            try{
+                Thread.sleep(1000);
+                mRandomNumber=new Random().nextInt(MAX)+MIN;
+                Log.d(TAG, "Thread_ID=="+Thread.currentThread().getId()+"  RandomNumber = "+i++);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopRandomNumberGenerator();
+    }
+protected class MyServiceBinder extends Binder {
+    public MyService getServiceInstance(){
+        return MyService.this;
+    }
+}
+}
