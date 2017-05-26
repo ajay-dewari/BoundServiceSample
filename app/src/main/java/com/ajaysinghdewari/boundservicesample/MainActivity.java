3376c8eb2b08950ef03ccd7242f752513cb15905
ajaydewari <ajdewari@gmail.com>
2017-05-26 23:52:33 +0530

    Initial commit

--- app/src/main/java/com/ajaysinghdewari/boundservicesample/MainActivity.java
+++ app/src/main/java/com/ajaysinghdewari/boundservicesample/MainActivity.java
@@ -38,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         btnStartService.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
